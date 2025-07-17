56063ac0557b43af8545f552dd13ca1d5c9ddccd
Agon Dragobuzhda <gpskosova@gmail.com>
2025-07-17 16:58:51 +0000

    Project Init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+function App() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <Button variant="outline">Test</Button>
+    </div>
+  );
+}
+
+export default App;
