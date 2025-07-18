f1d3ec3496a4851ec848e8dc96fbad7990ef142c
agondr <gpskosova@gmail.com>
2025-07-18 08:57:37 +0000

    Testing...

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <Button variant="outline">Test</Button>
+      <h1>testing...</h1>
     </div>
   );
 }
